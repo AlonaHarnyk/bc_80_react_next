32f0b298d9a2edbd27a0bf9aa849ec0be848313f
vivtorochok <vika17052001@gmail.com>
2026-05-18 17:54:07 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -61,19 +61,122 @@ car.color = 'orange';
 
 // number|string
 
-interface User {
-  name: string;
-  email: string;
+// interface User {
+//   name: string;
+//   email: string;
+// }
+
+// let user: null | User = null;
+
+// user = {
+//   name: 'Vlad',
+//   email: 'vlados@gmail.com',
+// };
+
+// type Status = 'pending' | 'fulfilled' | 'rejected';
+
+// let status: Status = 'pending';
+// status = 'fulfilled';
+
+//array
+const numbers: number[] = [1, 2, 3];
+// const numbers: Array<number> = [1, 2, 3];
+
+// const arr1: (number | string)[] = [56, "word", 46];
+
+// type Values = boolean | number | string;
+
+// const arr2: Values[] = [true, "hello", 43, false];
+
+
+// interface User {
+//   name: string;
+//   age: number;
+
+// }
+// const arr3: User[] = [
+//   { name: "Ivan", age: 21 },
+//   { name: "Petro", age: 26 },
+// ];
+
+//functions
+
+// 1. Просте додавання чисел
+
+// Умова:
+// Напиши функцію add, яка приймає два числа та повертає їх суму.
+
+const add = (x: number, y: number): number => {
+  return x + y;
 }
 
-let user: null | User = null;
+add(1, 2);
 
-user = {
-  name: 'Vlad',
-  email: 'vlados@gmail.com',
-};
+// 2. Привітання користувача
 
-type Status = 'pending' | 'fulfilled' | 'rejected';
+// Умова:
+// Функція greetUser приймає ім’я (string) і логічне значення isMorning (boolean).
+// Повертає рядок "Good morning, {name}" якщо isMorning === true, і "Hello, {name}" якщо false.
 
-let status: Status = 'pending';
-status = 'fulfilled';
+const greetUser = (name: string, isMorning: boolean): string => {
+  if (isMorning) {
+    return `Good morning, ${name}`;
+  } 
+  return `Hello, ${name}`;
+  
+
+}
+
+greetUser('Olexandr', true);
+
+
+// 3. Перевірка числа
+
+// Умова:
+// Функція isPositive приймає будь-яке значення (unknown) і повертає boolean:
+
+// true якщо це число > 0,
+
+// false якщо число ≤ 0 або не число.
+
+const isPositive = (arg: unknown): boolean => {
+  if (typeof arg !== "number") {
+    return false;
+  }
+  return arg > 0;
+}
+
+isPositive(5);
+
+
+// 4. Об’єкт користувача
+
+// Умова:
+// Створи інтерфейс User з полями:
+
+// name: string
+
+// age: number
+
+// email?: string (optional)
+
+// Функція getUserSummary приймає об’єкт User та повертає рядок "Name: {name}, Age: {age}, Email: {email}".
+// Якщо email немає — виводимо "Email: N/A".
+
+interface User {
+  name: string;
+  age: number;
+  email?: string;
+}
+
+const getUserSummary = ({ name, age, email }: User): string => {
+ 
+  return `Name: ${name}, Age: ${age}, Email: ${email ?? "N/A"}`
+}
+
+
+const user11 = {
+  name: "Anny",
+  age: 25,
+}
+getUserSummary(user11);
