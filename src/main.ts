7841cee78f1b071a47b97ecef1ce2389c319707d
d-levchenko <dl.qubb@gmail.com>
2026-05-18 18:28:57 +0300

    some tasks; added prettier

--- src/main.ts
+++ src/main.ts
@@ -88,7 +88,6 @@ const numbers: number[] = [1, 2, 3];
 
 // const arr2: Values[] = [true, "hello", 43, false];
 
-
 // interface User {
 //   name: string;
 //   age: number;
@@ -108,7 +107,7 @@ const numbers: number[] = [1, 2, 3];
 
 const add = (x: number, y: number): number => {
   return x + y;
-}
+};
 
 add(1, 2);
 
@@ -121,14 +120,11 @@ add(1, 2);
 const greetUser = (name: string, isMorning: boolean): string => {
   if (isMorning) {
     return `Good morning, ${name}`;
-  } 
+  }
   return `Hello, ${name}`;
-  
-
-}
+};
 
 greetUser('Olexandr', true);
-
 
 // 3. Перевірка числа
 
@@ -140,14 +136,13 @@ greetUser('Olexandr', true);
 // false якщо число ≤ 0 або не число.
 
 const isPositive = (arg: unknown): boolean => {
-  if (typeof arg !== "number") {
+  if (typeof arg !== 'number') {
     return false;
   }
   return arg > 0;
-}
+};
 
 isPositive(5);
-
 
 // 4. Об’єкт користувача
 
@@ -170,13 +165,112 @@ interface User {
 }
 
 const getUserSummary = ({ name, age, email }: User): string => {
- 
-  return `Name: ${name}, Age: ${age}, Email: ${email ?? "N/A"}`
-}
-
+  return `Name: ${name}, Age: ${age}, Email: ${email ?? 'N/A'}`;
+};
 
 const user11 = {
-  name: "Anny",
+  name: 'Anny',
   age: 25,
-}
+};
 getUserSummary(user11);
+
+// 5. Масив чисел
+
+// Умова:
+// Функція sumArray приймає масив чисел number[] і повертає їхню суму.
+
+const sumArray = (array: number[]): number => {
+  return array.reduce((acc, elem) => {
+    return acc + elem;
+  }, 0);
+};
+
+sumArray([1, 2, 3, 4, 5]);
+
+// 6. Масив рядків або чисел
+
+// Умова:
+// Функція joinArray приймає масив, елементи якого можуть бути string або number ((string | number)[])
+// і повертає рядок з усіх елементів, розділених комою.
+const joinArray = (array: (string | number)[]) => {
+  return array.join(', ');
+};
+
+joinArray([1, 1, 2, 'hello']);
+
+// 7. Функція з readonly полем
+
+// Умова:
+// Інтерфейс Point:
+
+interface Point {
+  readonly x: number;
+  readonly y: number;
+}
+
+// Функція distanceFromOrigin приймає Point і повертає відстань до початку координат (Math.sqrt(x*x + y*y)).
+const distanceFromOrigin = ({ x, y }: Point): number => {
+  return Math.sqrt(x * x + y * y);
+};
+
+distanceFromOrigin({ x: 1, y: 3 });
+
+// 8. Фільтр користувачів за віком
+
+// Умова:
+// Інтерфейс User2:
+
+interface User2 {
+  name: string;
+  age: number;
+  isAdmin?: boolean;
+}
+
+// Функція filterAdults приймає масив User2[] і повертає масив користувачів віком ≥ 18.
+const filterAdults = (array: User2[]) => {
+  return array.filter(({ age }) => age >= 18);
+};
+
+filterAdults([{ name: 'Ivan', age: 25, isAdmin: false }]);
+
+// 9. Об’єднання union типів
+
+// Умова:
+// Функція formatValue приймає параметр value: string | number | boolean і повертає рядок:
+
+// якщо boolean — "true"/"false",
+
+// якщо number — "Number: {value}",
+
+// якщо string — "String: {value}".
+const formatValue = (value: string | number | boolean): string => {
+  if (typeof value === 'boolean') {
+    return value ? 'true' : 'false';
+  }
+
+  if (typeof value === 'number') return `Number: ${value}`;
+
+  return `String: ${value}`;
+};
+
+formatValue('hello');
+formatValue(15);
+
+// 10. Масив невідомого типу
+
+// Умова:
+// Функція countNumbers приймає масив unknown[] і повертає кількість чисел у масиві.
+const countNumbers = (array: unknown[]): number => {
+  // let counter = 0;
+
+  // for (const elem of array) {
+  //   if (typeof elem === 'number') {
+  //     counter++;
+  //   }
+  // }
+
+  // return counter;
+  return array.filter(elem => typeof elem === 'number').length;
+};
+
+countNumbers([1, 2, 4, 'hello', [1, 2, 3]]);
